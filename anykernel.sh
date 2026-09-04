49575c5ba5656b92a54960961da49b5630b6bdef
Youff <asa.jazal@gmail.com>
2026-09-04 04:05:41 +0000

    ci: add AnyKernel3 flashable zip packaging
    Clone osm0sis/AnyKernel3, configure anykernel.sh for Xiaomi MiAtoll
    (non-A/B slot device with dtbo), copy Image.gz and dtbo.img into the
    AnyKernel3 template, and create a flashable zip. The zip is uploaded
    as a separate artifact alongside the raw kernel images.
    
    AnyKernel3 automatically detects and retains Magisk/KernelSU root.
    
    Signed-off-by: Youffx <asa.jazal@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,48 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Youffx Kernel for Xiaomi MiAtoll
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=miatoll
+device.name2=gram
+device.name3=joyeuse
+device.name4=excalibur
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot; # skip unpack/repack ramdisk
+
+flash_boot;
+## end boot install
+
+## dtbo install
+flash_generic dtbo;
+## end dtbo install
